9173fb290360f8421ed5c304241f3c0e8a0d4ec5
Marcus Hammarberg <marcusoft.net@gmail.com>
2016-03-05 21:59:26 +0100

    Blog post written

--- index.js
+++ index.js
@@ -1,10 +1,98 @@
-/* global require, module */
-var ApiBuilder = require("claudia-api-builder");
-var api = new ApiBuilder();
+/*global require, module*/
+var ApiBuilder = require('claudia-api-builder'),
+    api = new ApiBuilder(),
+    Promise = require('bluebird'),
+    AWS = require("aws-sdk"),
+    DOC = require("dynamodb-doc");
 
-api.get("/users/{id}", function (request) {
+// Create a promisified version of the docClient 
+var docClient = Promise.promisifyAll(new DOC.DynamoDB())
+
+// Export the api 
+module.exports = api;
+
+// Create a new user
+api.post('/users', function(request) {
 	'use strict';
-	var name = request.pathParams.id;
+    
+    var params = {
+        TableName: getTableName(request),
+        Item: {
+            userId: request.body.userId,
+            name: request.body.name,
+            age: request.body.age
+        }
+    };
 
-	return "Returning user with id:" + id;
+    return docClient.putItemAsync(params);
+
+}, { success: 201 }); // Return HTTP status 201 - Created when successful
+
+// get user for {id}
+api.get('/users/{id}', function(request) {
+    'use strict';
+    
+    // Set up parameters for dynamo
+    var params = {
+        TableName: getTableName(request),
+        Key: {
+            userId: request.pathParams.id
+        }
+    };
+
+    // Get the item using our promisified function
+    return docClient.getItemAsync(params);
+
 });
+
+// update the user name and age
+api.put('/users/{id}', function(request) {
+	'use strict';
+    
+    var params = {
+        TableName: getTableName(request),
+        Key: {
+            userId: request.pathParams.id
+        },
+		UpdateExpression: "set #name = :n, age = :a",
+		ExpressionAttributeNames : {"#name" : "name" },
+		ExpressionAttributeValues: {
+		    ":n":request.body.name,
+		    ":a":request.body.age
+		}
+    };
+
+    return docClient.updateItemAsync(params);
+
+}); //200 ok is standard for non-errors
+
+// delete user with {id}
+api.delete('/users/{id}', function(request) {
+	'use strict';
+    
+    // Set up parameters for dynamo
+    var params = {
+        TableName: getTableName(request),
+        Key: {
+            userId: request.pathParams.id
+        }
+    };
+
+	// Get the item using our promisified function
+    // return a nice little message in the .then-clause
+    return docClient.deleteItemAsync(params)
+        .then(function(data) {
+            return "Deleted user with id '" + request.pathParams.id + "'";
+        });
+
+}); //200 ok is standard for non-errors
+
+function getTableName(request) {
+    // The table name is stored in the Lambda stage variables
+    // Go to https://console.aws.amazon.com/apigateway/home/apis/[YOUR API ID]/stages/latest
+    // and click Stages -> latest -> Stage variables
+
+    // These values will be found under request.env
+    // Here's I'll use a default if not set
+    return request.env.tableName || "lambda-users";
+};
---
+++ testrun.sh
@@ -0,0 +1,21 @@
+echo "Create new user"
+curl -H "Content-Type: application/json" -X POST -d "{'userId' : '123', 'name' : 'Marcus Hammarberg', 'age' : 43 }" https://088zr0agp9.execute-api.us-west-2.amazonaws.com/latest/users
+
+echo
+echo "Get the created user"
+curl -H "Content-Type: application/json" https://088zr0agp9.execute-api.us-west-2.amazonaws.com/latest/users/123
+
+echo
+echo "Update user"
+curl -H "Content-Type: application/json" -X PUT -d "{'userId' : '123', 'name' : 'Carl Marcus Hammarberg', 'age' : 44 }" https://088zr0agp9.execute-api.us-west-2.amazonaws.com/latest/users/123 
+
+echo
+echo "Get updated user"
+curl -H "Content-Type: application/json" https://088zr0agp9.execute-api.us-west-2.amazonaws.com/latest/users/123
+
+echo
+echo "Delete user"
+curl -X DELETE https://088zr0agp9.execute-api.us-west-2.amazonaws.com/latest/users/123
+
+echo
+echo "Test run done... I should proabaly have done this in Javascript, huh?"
